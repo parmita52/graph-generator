701775ddc28686f9b688a0cb20fa787dbac2aea0
parmita52 <parmita52@gmail.com>
2019-06-07 20:54:23 -0700

    initial commit

---
+++ generator.py
@@ -0,0 +1,48 @@
+"""
+Good comment explaining what we wanna do over here
+
+Copyright: Parmita & Sayan  (:P)
+
+
+Input parameter Ideas:
+
+ - number of nodes
+ - number of edges
+ - ratio of nodes to edges
+ - average degree of vertex
+ - connected OR disconnected
+ - multigraph OR not multigraph
+ - output format (first try default SML for CMU students)
+    --> stretch goal use fuzzy logic/ML to determine format from an example
+        (just a random idea--might take a long while to actually make it work)
+
+"""
+
+#########################
+######## Imports ########
+#########################
+"""
+Network-x
+Graph-Tool
+gotta decide on good packages or implement own from scratch 
+"""
+
+#########################
+### Helper Functions ####
+#########################
+
+def user_input_basic():
+    pass
+
+def generate_graph(n_nodes=10, n_edges=10, edge_node_ratio=2, avg_degree=2, is_connected=1, output_format='SML'):
+    pass
+
+#########################
+##### Main Function #####
+#########################
+
+def main():
+    print("sup main")
+
+if __name__ == "__main__":
+    main()
