82892b1039fca8a81182b6f482a3c3983fa7d2a0
Parmita Bawankule <parmita@Parmitas-MacBook-Pro-2.local>
2019-06-12 08:00:22 -0700

    read in json

--- generator.py
+++ generator.py
@@ -1,40 +1,26 @@
-"""
-Good comment explaining what we wanna do over here
-
-Copyright: Parmita & Sayan  (:P)
-
-
-Input parameter Ideas:
-
- - number of nodes
- - number of edges
- - ratio of nodes to edges
- - average degree of vertex
- - connected OR disconnected
- - multigraph OR not multigraph
- - output format (first try default SML for CMU students)
-    --> stretch goal use fuzzy logic/ML to determine format from an example
-        (just a random idea--might take a long while to actually make it work)
-
-"""
-
 #########################
 ######## Imports ########
 #########################
-"""
-Network-x
-Graph-Tool
-gotta decide on good packages or implement own from scratch 
-"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import typing
+import json
 
 #########################
 ### Helper Functions ####
 #########################
 
-def user_input_basic():
+def user_input_json(pathname):
+    with open(pathname) as json_file:
+        data = json.loads(json_file.read())
+        print(data)
+
+def generate_graph():
     pass
 
-def generate_graph(n_nodes=10, n_edges=10, edge_node_ratio=2, avg_degree=2, is_connected=1, output_format='SML'):
+
+def draw_graph():
     pass
 
 #########################
@@ -42,7 +28,7 @@ def generate_graph(n_nodes=10, n_edges=10, edge_node_ratio=2, avg_degree=2, is_c
 #########################
 
 def main():
-    print("sup main")
+    user_input_json("./samples/sample1.json")
 
 if __name__ == "__main__":
     main()
