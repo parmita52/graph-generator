f5e7aa6f5ff1bf11e590ee6c9aa60ded583e8493
Sayan Chaudhry <sayan@sayans-mbp.lan>
2019-06-22 12:03:08 -0700

    refactor and update .gitignore

--- generator.py
+++ generator.py
@@ -8,14 +8,18 @@ import json
 import random
 import networkx as nx
 
+#########################
+####### Constants #######
+#########################
+
 WEIGHT_MIN_DEFAULT = 0
 WEIGHT_MAX_DEFAULT = 10
 NUM_NODES_DEFAULT = 10
 NUM_EDGES_DEFAULT = 10
-#########################
-### Helper Functions ####
-#########################
 
+#########################
+#### Data Structures ####
+#########################
 class UserInput:
     def __init__(self, numOfNodes, numOfEdges, isDirected, isMultigraph, hasSelfLoops, isWeighted, weightMin=WEIGHT_MIN_DEFAULT, weightMax=WEIGHT_MAX_DEFAULT, weightIsFloat=False):
         self.numOfNodes = numOfNodes
@@ -60,6 +64,36 @@ class UserInput:
         data = request.args.to_dict()
         return cls.fromJSON(data)
         
+#########################
+### Helper Functions ####
+#########################
+
+def add_weights(G, userInput):
+    for (source, target) in G.edges:
+        G[source][target]['weight'] = random.uniform(userInput.weightMin, userInput.weightMax) if userInput.weightIsFloat else random.randint(userInput.weightMin, userInput.weightMax)
+    return G
+
+# Remove self loops from edgeList
+def remove_self_loops(edgeList):
+    return list(filter(lambda x : x[0] != x[1], edgeList))
+
+# Remove directed edges from edgeList
+def remove_directed_edges(edgeList):
+    return list(filter(lambda x : x[0] >= x[1], edgeList))
+
+def draw_graph(G):
+    # print(G.edges)
+    for (a,b) in G.edges:
+        print(a, b, G[a][b]['weight'])
+    labels = nx.get_edge_attributes(G,'weight')
+    nx.draw(G, with_labels=True)
+    pos = nx.drawing.layout.spring_layout(G)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+    plt.show()
+
+#########################
+## Generator Functions ##
+#########################
 
 def generate_graph(userInput):
     D = userInput.isDirected
@@ -78,22 +112,9 @@ def generate_graph(userInput):
     
     if userInput.isWeighted:
         G = add_weights(G, userInput)
-        
+     
     # draw_graph(G)
     return G
-
-def add_weights(G, userInput):
-    for (source, target) in G.edges:
-        G[source][target]['weight'] = random.uniform(userInput.weightMin, userInput.weightMax) if userInput.weightIsFloat else random.randint(userInput.weightMin, userInput.weightMax)
-    return G
-
-# Remove self loops from edgeList
-def remove_self_loops(edgeList):
-    return list(filter(lambda x : x[0] != x[1], edgeList))
-
-# Remove directed edges from edgeList
-def remove_directed_edges(edgeList):
-    return list(filter(lambda x : x[0] >= x[1], edgeList))
 
 def generate_Graph(userInput, allEdges):
     if not(userInput.hasSelfLoops): 
@@ -130,128 +151,19 @@ def generate_MultiDiGraph(userInput, allEdges):
     G = nx.from_edgelist(finalEdges, create_using=nx.MultiDiGraph)
     return G
 
+#########################
+##### Main Function #####
+#########################
+
+def main():
+    test_sample()
+
 def test_sample():
+    # path = "./samples/sample1.json"
+    # path = "./samples/sample2.json"
     path = "./samples/sample3.json"
     userInput = UserInput.fromJSON(path)
     generate_graph(userInput)
-
-    # G = nx.Graph()
-    # ### TODO the possible classes are:
-    # # Graph, DiGraph, MultiGraph, or MultiDiGraph
-    # G.add_edges_from(generate_edge_list(userInput))
-    # draw_graph(G)
-
-def draw_graph(G):
-    # print(G.edges)
-    for (a,b) in G.edges:
-        print(a, b, G[a][b]['weight'])
-    labels = nx.get_edge_attributes(G,'weight')
-    nx.draw(G, with_labels=True)
-    pos = nx.drawing.layout.spring_layout(G)
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
-    plt.show()
-
-#########################
-##### Main Function #####
-#########################
-
-def main():
-    test_sample()
-    draw_graph(G)
-    return G
-
-def add_weights(G, userInput):
-    for (source, target) in G.edges:
-        G[source][target]['weight'] = random.uniform(userInput.weightMin, userInput.weightMax) if userInput.weightIsFloat else random.randint(userInput.weightMin, userInput.weightMax)
-    return G
-
-# Remove self loops from edgeList
-def remove_self_loops(edgeList):
-    return list(filter(lambda x : x[0] != x[1], edgeList))
-
-# Remove directed edges from edgeList
-def remove_directed_edges(edgeList):
-    return list(filter(lambda x : x[0] >= x[1], edgeList))
-
-def generate_Graph(userInput, allEdges):
-    if not(userInput.hasSelfLoops): 
-        allEdges = remove_self_loops(allEdges)
-    allEdges = remove_directed_edges(allEdges)
-    finalEdges = random.sample(allEdges, userInput.numOfEdges)
-    G = nx.from_edgelist(finalEdges, create_using=nx.Graph)
-    return G
-
-def generate_MultiGraph(userInput, allEdges):    
-    if not(userInput.hasSelfLoops): 
-        allEdges = remove_self_loops(allEdges)
-    allEdges = list(filter(lambda x : x[0] >= x[1], allEdges))
-    finalEdges = []
-    for _ in range(userInput.numOfEdges):
-        finalEdges.append(random.choice(allEdges))
-    G = nx.from_edgelist(finalEdges, create_using=nx.MultiGraph)
-    return G
-
-def generate_DiGraph(userInput, allEdges):
-    if not(userInput.hasSelfLoops): 
-        allEdges = remove_self_loops(allEdges)
-    finalEdges = random.sample(allEdges, userInput.numOfEdges)
-    G = nx.from_edgelist(finalEdges, create_using=nx.DiGraph)
-    return G
-
-def generate_MultiDiGraph(userInput, allEdges):
-    if not(userInput.hasSelfLoops): 
-        allEdges = remove_self_loops(allEdges)
-    allEdges = list(filter(lambda x : x[0] >= x[1], allEdges))
-    finalEdges = []
-    for _ in range(userInput.numOfEdges):
-        finalEdges.append(random.choice(allEdges))
-    G = nx.from_edgelist(finalEdges, create_using=nx.MultiDiGraph)
-    return G
-
-
-def generate_edge_list(userInput):
-    nodes = list(range(userInput.numOfNodes))
-    edge_list = [(0,1), (0,2), (1,2), (1,3), (2,3), (1,4), (3,4)]
-    return edge_list
-    # TODO decide to either create a list of edges and make Graph
-    # or loop through and add edges
-    # decide on NetworkX graph class early on?
-    # ^^ should this just make edge list or return whole graph?
-
-    ### check for numOfEdges vs averageDegree
-    ### check for isDirected
-    ### check for numOfConnectedComponents
-    ### check for isMultigraph
-    ### check for isTree
-    ### check for hasSelfLoops
-    ### check for weight
-
-
-def test_sample():
-    path = "./samples/sample3.json"
-    userInput = UserInput.fromPath(path)
-    generate_graph(userInput)
-
-    # G = nx.Graph()
-    # ### TODO the possible classes are:
-    # # Graph, DiGraph, MultiGraph, or MultiDiGraph
-    # G.add_edges_from(generate_edge_list(userInput))
-    # draw_graph(G)
-
-def draw_graph(G):
-    print(G.edges)
-    labels = nx.get_edge_attributes(G,'weight')
-    nx.draw(G, with_labels=True)
-    pos = nx.drawing.layout.spring_layout(G)
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
-    plt.show()
-
-#########################
-##### Main Function #####
-#########################
-
-def main():
-    test_sample()
 
 if __name__ == "__main__":
     main()
