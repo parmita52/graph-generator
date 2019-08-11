670d018d2694778f25c4ee2fa052fc8b33f819b8
Parmita Bawankule <parmita@Parmitas-MacBook-Pro-2.local>
2019-08-10 18:58:04 -0700

    fix user form issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,26 +16,6 @@ import Switch from '@material-ui/core/Switch';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-
-const currencies = [
-  {
-    value: 'USD',
-    label: '$',
-  },
-  {
-    value: 'EUR',
-    label: '€',
-  },
-  {
-    value: 'BTC',
-    label: '฿',
-  },
-  {
-    value: 'JPY',
-    label: '¥',
-  },
-];
-
 const useStyles = makeStyles(theme => ({
   container: {
     display: 'flex',
@@ -58,129 +38,135 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function OutlinedTextFields() {
-  const classes = useStyles();
-  var [values, setValues] = React.useState({
-  });
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numOfNodes: undefined,
+      numOfEdges: undefined,
+      isDirected: false,
+      isMultigraph: false,
+      hasSelfLoops: false,
+      hasWeights: false,
+      minWeight: undefined,
+      maxWeight: undefined,
+      isFloat: false,
+    };
 
-  const [state, setState] = React.useState({
-    isDirected: false,
-    isMultigraph: false,
-    hasSelfLoops: false,
-    weights: false,
-  });
+    this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.classes = this.classes.bind(this);
+  }
 
-  const { isDirected, isMultigraph, hasSelfLoops, weights} = state;
+  classes = useStyles;
 
-  const handleChange = name => event => {
-    setValues({ ...values, [name]: event.target.value });
-    setState({ ...state, [name]: event.target.checked });
-    console.log('hello')
-    console.log(values.weights)
+  handleChange = name => event => {
+    console.log(event.target.value)
+    this.setState({ ...this.state, [name]: event.target.checked });
   };
 
+  render() {
+    return (
+      <form className={this.classes.container} noValidate autoComplete="off">
+        <TextField
+          id="numOfNodes"
+          label="Number of Nodes"
+          value={this.state.numOfNodes}
+          onChange={this.handleChange('numOfNodes')}
+          type="number"
+          className={this.classes.textField}
+          margin="normal"
+          variant="outlined"
+        />
 
-  return (
-    
-    <form className={classes.container} noValidate autoComplete="off">
-      <TextField
-        id="numOfNodes"
-        label="Number of Nodes"
-        value={values.numOfNodes}
-        onChange={handleChange('age')}
-        type="number"
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-      />
-      <Divider variant="middle" component='form'/>
+        <Divider variant="middle" component='form'/>
 
-      <TextField
-        id="numOfEdges"
-        label="Number of Edges"
-        value={values.numOfEdges}
-        onChange={handleChange('age')}
-        type="number"
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-      />
+        <TextField
+          id="numOfEdges"
+          label="Number of Edges"
+          value={this.state.numOfEdges}
+          onChange={this.handleChange('numOfEdges')}
+          type="number"
+          className={this.classes.textField}
+          margin="normal"
+          variant="outlined"
+        />
 
-      <Divider variant="middle" />
+        <Divider variant="middle" />
 
-      <FormControl component="fieldset" className={classes.formControl}>
-        <FormLabel component="legend"></FormLabel>
-        <FormGroup>
-          <FormControlLabel
-            control={<Checkbox checked={isDirected} onChange={handleChange('isDirected')} value={values.isDirected} />}
-            label="is directed"
-          />
-          <FormControlLabel
-            control={<Checkbox checked={isMultigraph} onChange={handleChange('isMultigraph')} value={values.isMultigraph}/>}
-            label="is multigraph"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={hasSelfLoops} onChange={handleChange('hasSelfLoops')} value={values.hasSelfLoops}/>
-            }
-            label="has self loops"
-          />
-        </FormGroup>
-        <FormHelperText></FormHelperText>
-      </FormControl>
-
-      <FormControl component="fieldset" className={classes.formControl}>
-        <FormLabel component="legend"></FormLabel>
-        <FormGroup>
-          <FormControlLabel
-            id='weights'
-            control={<Checkbox checked={weights} onChange={handleChange('weights')} value={values.weights} />}
-            label="has weights"
-          />
-          <TextField
-            disabled
-            id="minWeight"
-            label="minWeight"
-            value={values.minWeight}
-            onChange={handleChange('minWeight')}
-            type="number"
-            className={classes.textField}
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            disabled={values.weights}
-            id="maxWeight"
-            label="maxWeight"
-            value={values.maxWeight}
-            onChange={handleChange('maxWeight')}
-            type="number"
-            className={classes.textField}
-            margin="normal"
-            variant="outlined"
-          />
-
-        <Typography component="div">
-        <Grid component="label" container alignItems="center" spacing={1}>
-          <Grid item>Integers</Grid>
-          <Grid item>
-            <Switch
-              disabled
-              checked={state.checkedC}
-              onChange={handleChange('checkedC')}
-              value={values.isFloat}
+        <FormControl component="fieldset" className={this.classes.formControl}>
+          <FormLabel component="legend"></FormLabel>
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox checked={this.state.isDirected} onChange={this.handleChange('isDirected')} value={this.state.isDirected} />}
+              label="is directed"
             />
+            <FormControlLabel
+              control={<Checkbox checked={this.state.isMultigraph} onChange={this.handleChange('isMultigraph')} value={this.state.isMultigraph}/>}
+              label="is multigraph"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox checked={this.state.hasSelfLoops} onChange={this.handleChange('hasSelfLoops')} value={this.state.hasSelfLoops}/>
+              }
+              label="has self loops"
+            />
+          </FormGroup>
+          <FormHelperText></FormHelperText>
+        </FormControl>
+
+        <FormControl component="fieldset" className={this.classes.formControl}>
+          <FormLabel component="legend"></FormLabel>
+          <FormGroup>
+            <FormControlLabel
+              id='weights'
+              control={<Checkbox checked={this.state.hasWeights} onChange={this.handleChange('hasWeights')} value={this.state.hasWeights} />}
+              label="has weights"
+            />
+            <TextField
+              disabled={this.state.hasWeights}
+              id="minWeight"
+              label="minWeight"
+              value={this.state.minWeight}
+              onChange={this.handleChange('minWeight')}
+              type="number"
+              className={this.classes.textField}
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              disabled={this.state.hasWeights}
+              id="maxWeight"
+              label="maxWeight"
+              value={this.state.maxWeight}
+              onChange={this.handleChange('maxWeight')}
+              type="number"
+              className={this.classes.textField}
+              margin="normal"
+              variant="outlined"
+            />
+
+          <Typography component="div">
+          <Grid component="label" container alignItems="center" spacing={1}>
+            <Grid item>Integers</Grid>
+            <Grid item>
+              <Switch
+                disabled={this.state.hasWeights}
+                checked={this.state.isFloat}
+                onChange={this.handleChange('isFloat')}
+                value={this.state.isFloat}
+              />
+            </Grid>
+            <Grid item>Decimal</Grid>
           </Grid>
-          <Grid item>Decimal</Grid>
-        </Grid>
-      </Typography>
+        </Typography>
 
-        </FormGroup>
-        <FormHelperText></FormHelperText>
-      </FormControl>
-
-      
-
-    </form>
-  );
+          </FormGroup>
+          <FormHelperText></FormHelperText>
+        </FormControl>
+      </form>
+    );
+  };
 }
+
+export default App;
